b6f38e11bb2e0c42216cb6fa12ff54dee923b8cb
meligg00 <68926638+meligg00@users.noreply.github.com>
2020-08-20 21:53:51 -0300

    buscador

--- js/products.js
+++ js/products.js
@@ -136,5 +136,23 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
 
         showCategoriesList();
+
     });
+
+    
+        function myFunction() {
+            input = document.getElementById("myInput");
+            var infoclass = document.getElementsByClassName('mb-1')
+            for (i = 0; i < infoclass.length; i++) {}
+                
+                a = infoclass[i]
+                txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    infoclass[i].style.display = "";
+                } else {
+                    infoclass[i].style.display = "none";
+                }
+            }
+        }
+        
 });
