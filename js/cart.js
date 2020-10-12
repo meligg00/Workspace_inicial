1e0b9dd10ebfbecc2383f9857cc80e61241e828d
meligg00 <68926638+meligg00@users.noreply.github.com>
2020-10-12 01:45:31 -0300

    carrito primera parte

--- js/cart.js
+++ js/cart.js
@@ -2,5 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok") {
+
+        mecart = resultObj.data;
+           
+
+                let nameysrcA  = document.getElementById("Art");
+                let countArt = document.getElementById("countA");
+                let costoA = document.getElementById("costA");
+            
+                nameysrcA.innerHTML = `<img src="`+mecart.articles[0].src+`" class="img-thumbnail" width="22%">`+ mecart.articles[0].name;
+                countArt.innerHTML = mecart.articles[0].count;
+                costoA.innerHTML = mecart.articles[0].unitCost+ " " + mecart.articles[0].currency;
+           
+                let totalP = document.getElementById("totalparcial");
+                totalP.innerHTML = (mecart.articles[0].count * mecart.articles[0].unitCost);
+                
+                
+        };
+            
+     });
+
+    
 
 });
