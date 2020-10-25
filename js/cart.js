0fa3bdf0cd6a2a8db11c3a8ed6674dd9b5af1220
meligg00 <68926638+meligg00@users.noreply.github.com>
2020-10-25 05:34:19 -0300

    dinamismo desaf.

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
+var porcentaje = 0.15 ;
+var datosCart= [];
 
 function myPago() {
     var option_value = document.getElementById("select").value;
@@ -9,99 +9,122 @@ function myPago() {
      }
 };
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CART_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok") {
 
-         mecart = resultObj.data;
+function showArt (artículos){
+
+    let tBody="";
+
+    for ( i=0; i<artículos.length; i++){         
+    
            
+           let totalPP = artículos[i].count * artículos[i].unitCost;
 
-                let nameysrcA  = document.getElementById("Art");
-                let countArt = document.getElementById("countA");
-                let costoA = document.getElementById("costA");
-            
-                nameysrcA.innerHTML = `<img src="`+mecart.articles[0].src+`" class="img-thumbnail" width="22%">`+ mecart.articles[0].name;
-                countArt.innerHTML = `<input id="inputCountA" class="quantity" min="0" name="quantity" style="width: 60px;" onchange value="`+ mecart.articles[0].count +`" type="number">`;
-                costoA.innerHTML = mecart.articles[0].unitCost+ " " + mecart.articles[0].currency;
-           
-                let totalP = document.getElementById("totalparcial");
-                let totalPP = mecart.articles[0].count * mecart.articles[0].unitCost;
-                totalP.innerHTML = totalPP + " " + mecart.articles[0].currency;
-                
-                let subTot= document.getElementById("subTotal")
-                subTot.innerHTML = totalPP + " " + mecart.articles[0].currency;
 
-                let totalF= document.getElementById("total");
-                totalF.innerHTML = totalPP + " " + mecart.articles[0].currency;
-
-                
-                var countArtNew = 0;
-
-            document.getElementById("inputCountA").addEventListener("change",function(){
-
-             let varCountA =document.getElementById("inputCountA").value;
-            countArtNew = varCountA * mecart.articles[0].unitCost;
-            
-             totalP.innerHTML = countArtNew + " " + mecart.articles[0].currency;
-             subTot.innerHTML = countArtNew + " " + mecart.articles[0].currency;
-             totalF.innerHTML= countArtNew + " "+ mecart.articles[0].currency;
-
-            
-            });
-
-            document.getElementById("tienda").addEventListener("change",function(){
-             
-                if (countArtNew != 0){
-                    document.getElementById("costoEnvio").innerHTML = "---";
-                    document.getElementById("total").innerHTML = countArtNew + " " + mecart.articles[0].currency;
-                  }else{
-                    document.getElementById("costoEnvio").innerHTML = "---";
-                    document.getElementById("total").innerHTML = totalPP + " " + mecart.articles[0].currency;
-                }
-                
-              });
-
-             document.getElementById("prem").addEventListener("change",function(){
-             
-                let porcentajeP = 0.15;
-                if (countArtNew != 0){
-                    document.getElementById("costoEnvio").innerHTML = countArtNew*porcentajeP +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = countArtNew*porcentajeP + countArtNew + " " + mecart.articles[0].currency;
-                 
-                  }else{
-                    document.getElementById("costoEnvio").innerHTML = totalPP*porcentajeP +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = totalPP*porcentajeP + totalPP + " " + mecart.articles[0].currency;
-                }
-
-              });
-             
-            document.getElementById("exp").addEventListener("change",function(){
-             
-                let porcentajeE = 0.07;
-                if (countArtNew != 0){
-                    document.getElementById("costoEnvio").innerHTML = Math.round(countArtNew*porcentajeE) +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = Math.round(countArtNew*porcentajeE) + countArtNew + " " + mecart.articles[0].currency;
-                  }else{
-                    document.getElementById( "costoEnvio").innerHTML = Math.round(totalPP*porcentajeE) +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = Math.round(totalPP*porcentajeE) + totalPP + " " + mecart.articles[0].currency;
-                }
-            });    
-
-            document.getElementById("std").addEventListener("change",function(){
-             
-                let porcentajeS = 0.05;
-                if (countArtNew != 0){
-                    document.getElementById("costoEnvio").innerHTML =  countArtNew*porcentajeS +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = countArtNew*porcentajeS + countArtNew + " " + mecart.articles[0].currency;
-                  }else{
-                    document.getElementById("costoEnvio").innerHTML = totalPP*porcentajeS +" "+ mecart.articles[0].currency;
-                    document.getElementById("total").innerHTML = totalPP*porcentajeS + totalPP + " " + mecart.articles[0].currency;
-                }
-
-            });
+           tBody+=`<tr id="Fila_`+ i +`">
         
-        }    
+           <th id="Art`+ i +`"><img src="`+ artículos[i].src+`" class="img-thumbnail" width="22%">`+ artículos[i].name+`</th>
+
+           <td id="entrega`+ i +`" style="text-align: justify;"><small><i class="fa fa-check-circle" style="color:rgb(66, 66, 240)"></i>Retiro en tienda <br><i style="color:rgb(66, 66, 240)" class="fa fa-check-circle"></i> Envío </small></td>
+       
+           <td style="text-align: center;"> <input onchange="actualizarTP(`+i+`)" id="inputCountA`+ i +`" class="quantity" min="0" name="quantity" style="width: 60px;" onchange value="`+ artículos[i].count +`" type="number"></td>
+       
+           <td id="costA`+ i +`" style="text-align: center;">`+artículos[i].unitCost+ " " + artículos[i].currency+`</td>
+       
+           <td id="totalparcial`+ i +`" style="text-align: center;">`+totalPP +" " + artículos[i].currency+`</td>
+
+           <td><button id="borrar`+ i +`" onclick="eliminarFila(`+i+`)" style="background-color:white;border-color: white;"><i style="font-size:24px" class="fa">&#xf1f8;</i></button></td> 
+
+           </tr>`;
+        
+    };   
+           document.getElementById("articlesWrapper").innerHTML += tBody;
+    
+};  
+
+
+ function eliminarFila(id) {
+    
+   document.getElementById("Fila_"+id).remove();
+  };
+
+ 
+
+ function actualizarTP(id){
+    let count = parseInt(document.getElementById("inputCountA"+id).value);
+    monedaProd = document.getElementById("costA"+id).textContent;
+    monedaProd2 = monedaProd.split(" ");
+ 
+ 
+    var totalP = monedaProd2[0] * count;
+ 
+   document.getElementById("totalparcial"+id).innerHTML = totalP +" "+ monedaProd2[1];
+ 
+   actualizaTotal();
+ 
+ };
+
+ 
+ function actualizaTotal(){
+    let subTotCostoHTML = document.getElementById("subTotal");
+    let costoEnvioHTML = document.getElementById("costoEnvio");
+    let costoTotHTML = document.getElementById("total");
+    let cotizacion = document.getElementById("dolar")
+  
+    var sumaSubTotales = 0;
+    
+
+    for (i=0; i<mecart.length; i++){
+      var textoTP = document.getElementById("totalparcial"+ i).textContent;
+      var arText = textoTP.split(" ");
+      var moneda = 1;
+     if (arText[1] == "USD"){
+        moneda = 43
+      }
+      var valorTexto = arText[0]*moneda;
+      sumaSubTotales += parseInt(valorTexto);
+    }
+  
+    let costoE = Math.round(porcentaje * sumaSubTotales);
+  
+    subTotCostoHTML.innerHTML = sumaSubTotales +" UYU";
+    cotizacion.innerHTML = "El dólar se cotiza a $43";
+    if (costoE != 0 ){
+        costoEnvioHTML.innerHTML = costoE + " UYU"}else{
+        costoEnvioHTML.innerHTML = "----"
+    };
+    costoTotHTML.innerHTML = (sumaSubTotales + costoE + " UYU");
+};
+ 
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(DESAF_AND_CART).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {     datoscart = resultObj.data;
+              mecart = datoscart.articles; 
+                    showArt(mecart);
+        };
     });
-     
+
+
+
+  document.getElementById("prem").addEventListener("change",function(){
+    porcentaje = 0.15;
+    actualizaTotal();
+  });
+  document.getElementById("exp").addEventListener("change",function(){
+    porcentaje= 0.07;
+    actualizaTotal();
+  });
+  document.getElementById("std").addEventListener("change",function(){
+    porcentaje = 0.05;
+    actualizaTotal();
+  });
+  document.getElementById("tienda").addEventListener("change",function(){
+    porcentaje = 0;
+    actualizaTotal();
+  });
 
 });
+
+
+          
+    
