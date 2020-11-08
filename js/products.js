f529664b83e57832132eb5c2d0665d892817b0ac
meligg00 <68926638+meligg00@users.noreply.github.com>
2020-11-08 18:27:33 -0300

    product responsive y grilla

--- js/products.js
+++ js/products.js
@@ -51,22 +51,16 @@ function showCategoriesList(){
       ){
                 
             htmlContentToAppend += `
-            <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ category.name +`</h4>
-                            <small class="text-muted">` + category.soldCount + ` artículos</small>
-                        </div>
-                        <p class="mb-1">` + category.description + ` <br>
-                        `+ category.cost + category.currency +`</p>
-    
-                    </div>
+            <div class="col-md-4">
+              <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+                <img class="bd-placeholder-img card-img-top"  src="` + category.imgSrc + `">
+                <h3 class="m-3">`+ category.name +`(`+ category.soldCount + `)</h3>
+                <div class="card-body">
+                  <p class="card-text">` + category.description + `<br>
+                  `+ category.cost + category.currency +`</p>
                 </div>
-            </a>
+              </a>
+            </div>
             `
         }
 
